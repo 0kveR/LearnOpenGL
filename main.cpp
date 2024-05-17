0dfdb0530783538532ade3acc481223941f4aa79
Krrish Verma <krrish583@gmail.com>
2024-05-17 18:04:15 -0400

    Prism

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 
 #include <iostream>
+#include <cmath>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
@@ -8,18 +9,19 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
 const char *vertexShaderSource = "#version 330 core\n"
                                  "layout (location = 0) in vec3 aPos;\n" // the position variable has attribute position 0
-                                 "out vec4 vertexColor;\n" // specify a color output to the fragment shader
+                                 "layout (location = 1) in vec3 aColor;\n" // the position variable has attribute position 0
+                                 "out vec3 ourColor;\n" // specify a color output to the fragment shader
                                  "void main()\n"
                                  "{\n"
                                  "    gl_Position = vec4(aPos, 1.0);\n" // see how we directly give a vec3 to vec4's constructor
-                                 "    vertexColor = vec4(0.5, 0.0, 0.0, 1.0);\n" // set the output variable to a dark-red color
+                                 "    ourColor = aColor;\n" // set the output variable to a dark-red color
                                  "}\n\0";
 const char *fragmentShaderSource = "#version 330 core\n"
                                    "out vec4 FragColor;\n"
-                                   "uniform vec4 ourColor;\n" // the input variable from the vertex shader (same name and same type)
+                                   "in vec3 ourColor;\n" // the input variable from the vertex shader (same name and same type)
                                    "void main()\n"
                                    "{\n"
-                                   "    FragColor = ourColor;\n"
+                                   "    FragColor = vec4(ourColor, 1.0  );\n"
                                    "}\n\0";
 
 int main() {
@@ -86,37 +88,28 @@ int main() {
     glDeleteShader(fragmentShader);
 
     float vertices[] = {
-//            0.5f,  0.5f, 0.0f,  // top right
-//            0.5f, -0.5f, 0.0f,  // bottom right
-//            -0.5f, -0.5f, 0.0f,  // bottom left
-//            -0.5f,  0.5f, 0.0f   // top left
-            -0.5f, -0.5f, 0.0f,
-            0.5f, -0.5f, 0.0f,
-            0.0f,  0.5f, 0.0f
-    };
-    unsigned int indices[] = {  // note that we start from 0!
-//            0, 1, 3,   // first triangle
-//            1, 2, 3    // second triangle
-            0, 1, 2
+            0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
+            -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
+            0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top
     };
 
-    unsigned int VBO, VAO, EBO;
+    unsigned int VBO, VAO;
 
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
 
     glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
+    glUseProgram(shaderProgram);
 
     // Prevent window from closing on its own
     while(!glfwWindowShouldClose(window)) {
@@ -126,9 +119,8 @@ int main() {
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         // Checks for any events and swaps buffers
         glfwSwapBuffers(window);
